5c483dc3f090f57d9b1c98fa563d8c72f2ce8eba
Sebastian <code@sebiann.moe>
2025-05-20 23:03:35 +0200

    Changed what you asked :)
    Signed-off-by: Sebastian <code@sebiann.moe>

--- src/main/kotlin/event/player/ItemFrameInteract.kt
+++ src/main/kotlin/event/player/ItemFrameInteract.kt
@@ -1,23 +1,16 @@
 package event.player
 
 import org.bukkit.Material
-import org.bukkit.entity.Entity
 import org.bukkit.entity.ItemFrame
 import org.bukkit.event.player.PlayerInteractEntityEvent
 
 object ItemFrameInteract {
-
     fun itemframeInteractEvent(event: PlayerInteractEntityEvent) {
-        val entity: Entity? = event.rightClicked
-
-        if (entity is ItemFrame) {
-            val itemFrame = entity
-
-            if (event.getPlayer().isSneaking() && event.getPlayer().getInventory().getItemInMainHand().getType() === Material.AIR) {
-                val newVisibility = !itemFrame.isVisible()
-                itemFrame.setVisible(newVisibility)
-                event.setCancelled(true)
-            }
+        val itemFrame = event.rightClicked as? ItemFrame ?: return
+        if (event.player.isSneaking && event.player.inventory.itemInMainHand.type === Material.AIR) {
+            val newVisibility = !itemFrame.isVisible()
+            itemFrame.setVisible(newVisibility)
+            event.setCancelled(true)
         }
     }
 }
--- src/main/kotlin/event/player/PlayerInteract.kt
+++ src/main/kotlin/event/player/PlayerInteract.kt
@@ -1,11 +1,9 @@
 package event.player
 
 import event.player.CampfireInteract.campfireInteractEvent
-import event.player.ItemFrameInteract.itemframeInteractEvent
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
-import org.bukkit.event.player.PlayerInteractEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
 
 class PlayerInteract : Listener {
@@ -20,13 +18,6 @@ class PlayerInteract : Listener {
             else -> {
                 // DONT CARE/ADD OTHER CASES
             }
-        }
-    }
-
-    @EventHandler
-    fun onPlayerInteract(event: PlayerInteractEntityEvent) {
-        if (event.hand.equals(org.bukkit.inventory.EquipmentSlot.HAND)) { // The reason for this is really funny
-            itemframeInteractEvent(event)
         }
     }
 }
