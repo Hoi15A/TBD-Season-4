45c82aa334182ee85ea4cd98ed364c7f19fee56b
Niklas <derNiklaas@users.noreply.github.com>
2025-06-20 23:56:14 +0200

    add who originally obtained the item

--- src/main/kotlin/event/player/EnderEyeInteract.kt
+++ src/main/kotlin/event/player/EnderEyeInteract.kt
@@ -107,7 +107,9 @@ class EnderEyeInteract: Listener {
     fun giveMemento(event: PlayerInteractEvent) {
         val player = event.player
         val trueEye = event.item!!
-        val newLore = mutableListOf("<white><!i>${ItemRarity.EPIC.rarityGlyph}${ItemType.MEMENTO.typeGlyph}", "<!i><yellow>You feel a strange energy emerging from within.").map { Formatting.allTags.deserialize(it) } + trueEye.itemMeta.lore()!![2]
+        val baseLore = listOf("<white><!i>${ItemRarity.EPIC.rarityGlyph}${ItemType.MEMENTO.typeGlyph}", "<!i><yellow>You feel a strange energy emerging from within.").map { Formatting.allTags.deserialize(it) }
+        val obtainedLore = listOf("", "<!i><grey>Earned by: <white>${event.player.name}").map { Formatting.allTags.deserialize(it) }
+        val newLore = baseLore + trueEye.itemMeta.lore()!![2] + obtainedLore
 
         val memento = ItemStack(Material.PLAYER_HEAD)
         val mementoMeta = memento.itemMeta as SkullMeta
