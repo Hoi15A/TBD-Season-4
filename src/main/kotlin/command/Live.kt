1c5245f931bd9f9ef5e36b6e1e4d5ab137ec37c5
Sebastian <code@sebiann.moe>
2025-07-31 12:11:53 +0200

    Made players name in tab purple while live and make it announce in chat when going live
    Signed-off-by: Sebastian <code@sebiann.moe>

--- src/main/kotlin/command/Live.kt
+++ src/main/kotlin/command/Live.kt
@@ -1,7 +1,10 @@
 package command
 
+import chat.ChatUtility
 import io.papermc.paper.command.brigadier.CommandSourceStack
-
+import net.kyori.adventure.audience.Audience
+import net.kyori.adventure.text.format.NamedTextColor.LIGHT_PURPLE
+import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import org.incendo.cloud.annotations.Command
 import org.incendo.cloud.annotations.CommandDescription
@@ -18,9 +21,15 @@ class Live {
         val player = css.sender as Player
         if (LiveUtil.livePlayers.containsKey(player)) {
             LiveUtil.livePlayers.remove(player)
+            player.displayName(null);
+            player.playerListName(null);
             player.sendMessage("Live mode disabled.")
         } else {
             LiveUtil.livePlayers[player] = true
+            ChatUtility.messageAudience(Audience.audience(Bukkit.getOnlinePlayers()), "<tbdcolour>${player.name} went Live", false)
+            val newName = player.displayName().color(LIGHT_PURPLE)
+            player.displayName(newName);
+            player.playerListName(newName);
             player.sendMessage("Live mode enabled.")
         }
     }
