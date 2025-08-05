181af88875d842b7d511dc2c845dfa765c1eeb65
Austin Albrecht <austin.aa@me.com>
2025-08-05 22:32:26 +0200

    More wip

--- src/main/kotlin/command/ShowStat.kt
+++ src/main/kotlin/command/ShowStat.kt
@@ -3,18 +3,25 @@ package command
 import chat.Formatting
 import fr.mrmicky.fastboard.adventure.FastBoard
 import io.papermc.paper.command.brigadier.CommandSourceStack
+import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.Component.text
 import net.kyori.adventure.text.format.NamedTextColor
+import net.kyori.adventure.text.format.NamedTextColor.BLUE
+import net.kyori.adventure.text.format.NamedTextColor.RED
+import net.kyori.adventure.text.format.TextDecoration
+import net.kyori.adventure.text.format.TextDecoration.UNDERLINED
 import org.bukkit.Bukkit
 import org.bukkit.entity.EntityType
 import org.bukkit.Material
 import org.bukkit.OfflinePlayer
 import org.bukkit.Statistic
 import org.bukkit.entity.Player
+import org.bukkit.scheduler.BukkitRunnable
 import org.incendo.cloud.annotations.Command
 import org.incendo.cloud.annotations.Flag
 import org.incendo.cloud.annotations.Permission
 import org.incendo.cloud.annotations.processing.CommandContainer
+import plugin
 import util.Sounds.ERROR_DIDGERIDOO
 import java.util.UUID
 
@@ -22,7 +29,8 @@ import java.util.UUID
 @CommandContainer
 class ShowStat {
 
-    val boards: Map<UUID, FastBoard> = mapOf()
+    val pageSize = 14
+    val secondsPerPage = 10
 
     @Command("showstat|sb <stat>")
     @Permission("tbd.command.echo")
@@ -39,10 +47,10 @@ class ShowStat {
             Bukkit.getServer().offlinePlayers
         }
 
-        val sbEntries = mutableListOf<Pair<String, Int>>()
+        val sbEntries = mutableListOf<Pair<Component, Int>>()
         when (stat.type) {
             Statistic.Type.UNTYPED -> {
-                sbEntries.addAll(players.map { Pair(it.name ?: "Unknown", it.getStatistic(stat)) }.toMutableList())
+                sbEntries.addAll(players.map { Pair(text(it.name ?: "Unknown"), it.getStatistic(stat)) }.toMutableList())
             }
             Statistic.Type.ITEM, Statistic.Type.BLOCK -> {
                 if (material == null) {
@@ -50,7 +58,7 @@ class ShowStat {
                     player.playSound(ERROR_DIDGERIDOO)
                     return
                 }
-                sbEntries.addAll(players.map { Pair(it.name ?: "Unknown", it.getStatistic(stat, material)) }.toMutableList())
+                sbEntries.addAll(players.map { Pair(text(it.name ?: "Unknown"), it.getStatistic(stat, material)) }.toMutableList())
             }
             Statistic.Type.ENTITY -> {
                 if (entityType == null) {
@@ -58,28 +66,54 @@ class ShowStat {
                     player.playSound(ERROR_DIDGERIDOO)
                     return
                 }
-                sbEntries.addAll(players.map { Pair(it.name ?: "Unknown", it.getStatistic(stat, entityType)) }.toMutableList())
+                sbEntries.addAll(players.map { Pair(text(it.name ?: "Unknown"), it.getStatistic(stat, entityType)) }.toMutableList())
             }
         }
 
         sbEntries.removeIf { it.second == 0 }
 
         val sum = sbEntries.sumOf { it.second }
-        sbEntries.addFirst(Pair("Total", sum))
+        sbEntries.addFirst(Pair(text("Total").color(BLUE).decorate(UNDERLINED), sum))
 
         val sorted = sbEntries.sortedByDescending { it.second }
 
+        val statScoreboardRunnable = object : BukkitRunnable() {
+            var pageIndex = 0
+            override fun run() {
+                val pages = sorted.chunked(pageSize)
+                if (pageIndex <= pages.lastIndex) {
+                    val page = pages[pageIndex].toMutableList()
+                    val title = text(stat.name).color(RED).append(text(" (${pageIndex+1}/${pages.size})"))
+                    broadcastScoreboardLines(title, page)
+                    pageIndex++
+                } else {
+                    clearScoreboards(20L)
+                    this.cancel()
+                }
+            }
+        }
+        statScoreboardRunnable.runTaskTimer(plugin, 0L, secondsPerPage * 10L)
+    }
+
+    private fun broadcastScoreboardLines(title: Component, lines: List<Pair<Component, Int>>) {
         for (player in Bukkit.getOnlinePlayers()) {
             val board = FastBoard(player)
-            board.updateTitle(text(stat.name).color(NamedTextColor.RED))
-            val names = sorted.map { text(it.first) }
-            val scores = sorted.map { text(it.second) }
+            board.updateTitle(title)
+            val names = lines.map { it.first }
+            val scores = lines.map { text(it.second).color(RED) }
             board.updateLines(names, scores)
         }
+    }
 
-        sorted.forEach {
-            player.sendMessage(text("${it.first} - ${it.second}"))
-        }
+    private fun clearScoreboards(delay: Long) {
+        object : BukkitRunnable() {
+            override fun run() {
+                for (player in Bukkit.getOnlinePlayers()) {
+                    val board = FastBoard(player)
+                    board.delete()
+                }
+            }
+        }.runTaskLater(plugin, delay)
     }
 
 }
