d6096580d433437eea2e82ec6abf5bf8e75ed31a
Niklas <derNiklaas@users.noreply.github.com>
2026-03-18 18:52:01 +0100

    Add reset command

--- src/main/kotlin/command/Height.kt
+++ src/main/kotlin/command/Height.kt
@@ -14,6 +14,12 @@ import org.incendo.cloud.annotations.processing.CommandContainer
 @CommandContainer
 class Height {
 
+    companion object {
+        const val MIN_HEIGHT = 120
+        const val MAX_HEIGHT = 250
+        const val DEFAULT_HEIGHT = 180
+    }
+
     @Command("height <cm>")
     @CommandDescription("Set your in-game scale based on your real-life height in cm.")
     @Permission("tbd.command.height")
@@ -30,13 +36,29 @@ class Height {
         applyHeight(sender, cm, target)
     }
 
+    @Command("height reset")
+    @CommandDescription("Reset your in-game scale to default.")
+    @Permission("tbd.command.height")
+    fun heightResetSelf(css: CommandSourceStack) {
+        val player = css.sender as? Player ?: return
+        applyHeight(player, DEFAULT_HEIGHT, player)
+    }
+
+    @Command("height reset <player>")
+    @CommandDescription("Reset another player's in-game scale to default.")
+    @Permission("tbd.command.height.others")
+    fun heightResetOther(css: CommandSourceStack, @Argument("player") target: Player) {
+        val sender = css.sender as? Player ?: return
+        applyHeight(sender, DEFAULT_HEIGHT, target)
+    }
+
     private fun applyHeight(sender: Player, cm: Int, target: Player) {
-        if (cm !in 120..250) {
-            sender.sendMessage(Formatting.allTags.deserialize("<red>Height must be between 120 and 250 cm."))
+        if (cm !in MIN_HEIGHT..MAX_HEIGHT) {
+            sender.sendMessage(Formatting.allTags.deserialize("<red>Height must be between $MIN_HEIGHT and $MAX_HEIGHT cm."))
             return
         }
 
-        val scale = cm / 180.0
+        val scale = cm / DEFAULT_HEIGHT.toDouble()
         val attribute = target.getAttribute(Attribute.SCALE) ?: return
         attribute.baseValue = scale
 
